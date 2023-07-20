c83743b259892ce688dc444f75a351671d757258
MSI <fuadbayramov.fb@gmail.com>
2023-07-20 13:19:35 -0800

    examAdmin

---
+++ exam-management/src/main/java/com/exammanagament/repository/AnswerRepository.java
@@ -0,0 +1,7 @@
+package com.exammanagament.repository;
+
+import com.exammanagament.entity.Answer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnswerRepository extends JpaRepository<Answer,Long> {
+}
