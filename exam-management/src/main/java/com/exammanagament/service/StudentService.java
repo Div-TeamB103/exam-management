042394846aa1037b83b81d5364d56d29a8dcc0e1
ferid <elmar@div.edu.az>
2023-07-21 15:31:20 +0400

    Student controller,service,serviceimpl

--- exam-management/src/main/java/com/exammanagament/service/StudentService.java
+++ exam-management/src/main/java/com/exammanagament/service/StudentService.java
@@ -1,4 +1,13 @@
 package com.exammanagament.service;
 
+import com.exammanagament.dto.StudentDto;
+
+import java.util.List;
+
 public interface StudentService {
+    StudentDto createStudent (StudentDto studentDto);
+    List<StudentDto>readAllStudents();
+    StudentDto updateStudentGetById(Long id,StudentDto studentDto);
+    String deleteStudentGetById(Long id);
+    StudentDto studentGetById(Long id);
 }
