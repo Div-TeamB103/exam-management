f83f7549a2fa998b3272bc8c5281ebe46b9f54f5
Emilmacbooku <emil.aliyev@div.edu.az>
2023-07-25 00:07:18 +0400

    Fixed Bug

--- exam-management/src/main/java/com/exammanagament/map/ParentMap.java
+++ exam-management/src/main/java/com/exammanagament/map/ParentMap.java
@@ -11,7 +11,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
-
+@Component
 @Mapper()
 public interface ParentMap {
     @Mapping(target = "studentsId", expression = "java(toSet(parent.getStudents()))")
