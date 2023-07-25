94adda9afb562401341a87981b0683383fc006d4
Elcan Mahmudov <elcan.m0707@gmail.com>
2023-07-25 15:55:24 +0400

    ParentController Fixed

---
+++ exam-management/src/main/java/com/exammanagament/exception/NotFoundUserException.java
@@ -0,0 +1,8 @@
+package com.exammanagament.exception;
+
+public class NotFoundUserException extends Exception{
+
+    public NotFoundUserException(String message){
+        super(message);
+    }
+}
--- exam-management/src/main/java/com/exammanagament/service/ParentServiceInterface.java
+++ exam-management/src/main/java/com/exammanagament/service/ParentServiceInterface.java
@@ -1,10 +1,17 @@
 package com.exammanagament.service;
 
 
+import com.exammanagament.dto.ParentDto;
+import com.exammanagament.exception.DublicateUserException;
+import com.exammanagament.exception.NotFoundUserException;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface ParentServiceInterface {
 
     public List<ParentDto> getAllParent() ;
-    public Optional<ParentDto> getParenyByEmail( String email) throws NullPointerException, NotFoundUserException;
+    public Optional<ParentDto> getParenyByEmail(String email) throws NullPointerException, NotFoundUserException;
     public ParentDto createParent(ParentDto parentDto) throws DublicateUserException;
     ParentDto updateParent(Long id) throws NotFoundUserException;
     ParentDto deleteParent(Long id) throws NotFoundUserException;
